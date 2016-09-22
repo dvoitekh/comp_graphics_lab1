1442874d63010e82751021cb47d439528df8bebc
d.voitekh <d.voitekh@mail.ru>
2016-09-22 10:54:57 +0300

    Initial Commit

---
+++ Lab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab1
+//
+//  Created by Dmitry Voitekh on 9/22/16.
+//  Copyright © 2016 Dmitry Voitekh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
